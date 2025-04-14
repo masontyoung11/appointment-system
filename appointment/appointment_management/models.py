7de5a68f6de9cc2ca2d3a67bb25ed7b7eb353501
masontyoung <masonyou19@gmail.com>
2025-04-14 21:10:49 +0100

    Add appointment model and enhance appointment creation form with energy usage and time selection

--- appointment/appointment_management/models.py
+++ appointment/appointment_management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Appointments(models.Model):
+    type = models.CharField(max_length=100)
+    energy_usage = models.FloatField()
+    time_choice = models.CharField(max_length=100)
+    date = models.DateField()
--- appointment/appointment_management/views.py
+++ appointment/appointment_management/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def create_appointment(request):
+    if request.method == "POST":
+        bedroom_to_energy_map = {
+            "low-bedrooms": 7500,
+            "medium-bedrooms": 11500,
+            "high-bedrooms": 17000,
+        }
+
+        appointment_type = request.POST.get('installation-type')
+        energy_usage = request.POST.get('bedroom-amm')
+        time_choice = request.POST.get('time-choice')
+        date = request.POST.get('date')
+
+        if energy_usage == "custom-bedrooms-input":
+            energy_usage = request.POST.get('custom-bedrooms-input')
+        else:
+            print(energy_usage)
+            energy_usage = bedroom_to_energy_map.get(energy_usage, 1)
+            print(energy_usage)
+
     return render(request, 'create_appointment.html')
