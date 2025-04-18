fc6a7f087141aeb36ba2aa37bcba299325c09574
masontyoung <masonyou19@gmail.com>
2025-04-18 14:02:06 +0100

    Enhance settings page with appointment management and improved layout

--- appointment/accountManagement/views.py
+++ appointment/accountManagement/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from appointment_management.models import Appointments
 
 # Create your views here.
 def settings(request):
-    return render(request, 'accountManagement/settings.html')
+    appointments = Appointments.objects.all()
+
+    return render(request, 'accountManagement/settings.html', {'appointments': appointments})
